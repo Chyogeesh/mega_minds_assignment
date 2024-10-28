b68f075e8d6ae12de392a6b214f5a4b934352fbd
Ch Yogeesh Sai <108213999+Chyogeesh@users.noreply.github.com>
2024-10-29 01:09:08 +0530

    Create students.js

---
+++ students.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { createStudent, getStudents } = require('../controllers/studentController');
+const router = express.Router();
+
+router.post('/', createStudent);
+router.get('/', getStudents);
+
+module.exports = router;
